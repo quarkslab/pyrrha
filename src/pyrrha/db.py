3a5f7fd55282cc7f7a390d294c63fd654f581f7a
Eloïse Brocas <ebrocas@quarkslab.com>
2023-05-29 16:34:15 +0200

    Initial commit

---
+++ src/pyrrha/db.py
@@ -0,0 +1,157 @@
+from pathlib import Path
+
+import sourcetraildb
+
+
+class DBInterface:
+    """TODO, rappeler obligation de init et close + commit"""
+
+    def __init__(self, db_path: Path, clear_db: bool = True):
+        """
+        Initialize the interface with the DB used by sourcetrail.
+        :param db_path: path of the db file, will create it if it does not exist
+        :param clear_db: if the DB should be cleared or not, default = true
+        """
+        if db_path.suffix != '.srctrldb':
+            db_path = db_path.with_suffix('.srctrldb')
+        self.db_path = db_path
+        sourcetraildb.open(str(self.db_path))
+        if clear_db:
+            sourcetraildb.clear()
+        sourcetraildb.beginTransaction()
+
+    @staticmethod
+    def close():
+        """
+        Close the connection to the DB after committing the last changes done by the user.
+        """
+        sourcetraildb.commitTransaction()
+        sourcetraildb.close()
+
+    @staticmethod
+    def __create_name_element_from_path(path: Path) -> str:
+        """Uniformize the name elements used to represent paths"""
+        return f'{{ "prefix": "{path.parent}/", "name": "{path.name}", "postfix": "" }} '
+
+    @staticmethod
+    def __create_name_element_from_name(symbol_name: str) -> str:
+        """Uniformize the name elements used to represent paths"""
+        return f'{{ "prefix": "", "name": "{symbol_name}", "postfix": "" }} '
+
+    @staticmethod
+    def __record_class(name_element: str) -> int:
+        """
+        Record a class symbol inside the current open DB.
+        This DB should be already open.
+        :param name_element: name element of the class as a JSON str of the form
+            f'{ "prefix": "class", "name": "MY NAME", "postfix": "()" }'
+            (prefix and postfixs are not mandatory)
+        :return: unique identifier of the created object inside the DB
+        """
+        cls_id = sourcetraildb.recordSymbol(
+            f'{{"name_delimiter": ":", "name_elements": [ {name_element}] }}')
+        sourcetraildb.recordSymbolKind(cls_id, sourcetraildb.SYMBOL_CLASS)
+        sourcetraildb.recordSymbolDefinitionKind(cls_id, sourcetraildb.DEFINITION_EXPLICIT)
+        return cls_id
+
+    @staticmethod
+    def __record_typedef(name_element: str) -> int:
+        """
+        Record a typedef symbol inside the current open DB.
+        This DB should be already open.
+        :param name_element: name element of the typedef as a JSON str of the form
+            f'{ "prefix": "void", "name": "MY NAME", "postfix": "()" }'
+            (prefix and postfixs are not mandatory)
+        :return: unique identifier of the created object inside the DB
+        """
+        cls_id = sourcetraildb.recordSymbol(
+            f'{{"name_delimiter": ":", "name_elements": [ {name_element}] }}')
+        sourcetraildb.recordSymbolKind(cls_id, sourcetraildb.SYMBOL_TYPEDEF)
+        sourcetraildb.recordSymbolDefinitionKind(cls_id, sourcetraildb.DEFINITION_EXPLICIT)
+        return cls_id
+
+    @staticmethod
+    def __record_method(class_name_element: str, method_name_element: str) -> int:
+        """
+        Record a typedef symbol inside the current open DB.
+        This DB should be already open.
+        :param class_name_element: name element of the parent class as a JSON str of the form
+            f'{ "prefix": "void", "name": "MY NAME", "postfix": "()" }'
+            (prefix and postfixs are not mandatory)
+        :param method_name_element: name element of the method
+        :return: unique identifier of the created object inside the DB
+        """
+        meth_id = sourcetraildb.recordSymbol(
+            f'{{"name_delimiter": ":", "name_elements": [ {class_name_element}, {method_name_element}] }}')
+        sourcetraildb.recordSymbolKind(meth_id, sourcetraildb.SYMBOL_METHOD)
+        sourcetraildb.recordSymbolDefinitionKind(meth_id, sourcetraildb.DEFINITION_EXPLICIT)
+        return meth_id
+
+    @staticmethod
+    def __record_field(class_name_element: str, field_name_element: str) -> int:
+        """
+        Record a field symbol inside the current open DB.
+        This DB should be already open.
+        :param class_name_element: name element of the parent class as a JSON str of the form
+            f'{ "prefix": "void", "name": "MY NAME", "postfix": "()" }'
+            (prefix and postfixs are not mandatory)
+        :param field_name_element: name element of the field
+        :return: unique identifier of the created object inside the DB
+        """
+        field_id = sourcetraildb.recordSymbol(
+            f'{{"name_delimiter": ":", "name_elements": [ {class_name_element}, {field_name_element}] }}')
+        sourcetraildb.recordSymbolKind(field_id, sourcetraildb.SYMBOL_METHOD)
+        sourcetraildb.recordSymbolDefinitionKind(field_id, sourcetraildb.DEFINITION_EXPLICIT)
+        return field_id
+
+    def record_binary_file(self, file_path: Path) -> int:
+        """
+        Add a representation of binary file into the DB
+        :param file_path: path of the file (inside the firmware)
+        :return: the uniq identifier of the file
+        """
+        return self.__record_class(self.__create_name_element_from_path(file_path))
+
+    def record_exported_symbol(self, file_path: Path, name: str, is_function: bool) -> int:
+        """
+        Add a representation of an exported symbol into the DB
+        :param file_path: path of the binary/library (inside the firmware)
+        :param name: symbol name
+        :param is_function: True if the symbol is a function, else False
+        :return: the uniq identifier of the exported symbol
+        """
+        if is_function:
+            return self.__record_method(self.__create_name_element_from_path(file_path),
+                                        self.__create_name_element_from_name(name))
+        return self.__record_field(self.__create_name_element_from_path(file_path),
+                                   self.__create_name_element_from_name(name))
+
+    def record_symlink(self, symlink_path: Path) -> int:
+        """
+        Add a representation of a symlink into the DB
+        :param symlink_path: path of the symlink to insert (inside the firmware)
+        :return: the uniq identifier of the symlink
+        """
+        return self.__record_typedef(self.__create_name_element_from_path(symlink_path))
+
+    @staticmethod
+    def record_symlink_target(symlink_id: int, target_id: int) -> None:
+        """
+        Add a link between the symlink object and its target object.
+        It is a reference with the type IMPORT.
+        :param symlink_id: id of the symlink object inside db
+        :param target_id: id of the symlink target object inside db
+        """
+        sourcetraildb.recordReference(symlink_id, target_id, sourcetraildb.REFERENCE_IMPORT)
+
+    @staticmethod
+    def record_import(current_object_id: int, imported_object_id: int) -> None:
+        """
+        Add a reference (with the type IMPORT) between the current object and
+        the imported target object.
+        :param current_object_id: object which import
+        :param imported_object_id: imported object
+        """
+        sourcetraildb.recordReference(current_object_id,
+                                      imported_object_id,
+                                      sourcetraildb.REFERENCE_IMPORT)
---
+++ src/pyrrha/mapper.py
@@ -0,0 +1,262 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import click
+import lief
+
+from .db import DBInterface
+
+
+@dataclass
+class ELFBinary:
+    path: Path
+    id: int = None
+    imported_lib_names: list[str] = field(default_factory=list)
+    imported_libs: list['ELFBinary'] = field(default_factory=list)
+    imported_symbols: list[str] = field(default_factory=list)  # list(symbol names) symbols and functions
+    version_requirement: dict[str, list[str]] = field(default_factory=dict)  # dict(symbol_name, list(requirements))
+    exported_symbol_ids: dict[str, int] = field(default_factory=dict)  # dict(name, id)
+    exported_function_ids: dict[str, int] = field(default_factory=dict)  # dict(name, id)
+
+    def __post_init__(self):
+        """
+        parse the given path with lief to automatically fill the other fields
+        at the exception done of the ids (the object should be put on a DB)
+        """
+        if self.name == 'libcrypto.so.1.1':
+            lief_obj = lief.ELF.parse(str(self.path), lief.ELF.DYNSYM_COUNT_METHODS.HASH)
+        else:
+            lief_obj = lief.ELF.parse(str(self.path))
+
+        # parse imported libs/symbols
+        self.imported_lib_names = lief_obj.libraries
+        self.imported_symbols = [s.name for s in lief_obj.imported_symbols]
+
+        # parse exported symbols
+        for s in lief_obj.exported_symbols:
+            if s.is_function:
+                self.exported_function_ids[s.name] = None
+            else:
+                self.exported_symbol_ids[s.name] = None
+
+        # parse version requirements
+        for req in lief_obj.symbols_version_requirement:
+            for symb in req.get_auxiliary_symbols():
+                if symb.name in self.version_requirement:
+                    self.version_requirement[symb.name].append(req.name)
+                else:
+                    self.version_requirement[symb.name] = [req.name]
+
+    @property
+    def name(self):
+        return self.path.name
+
+    def record_in_db(self, db: DBInterface) -> None:
+        """
+        Record the binary inside the given db as well as its exported
+        symbols/functions.
+        Update 'self.id' with the id of the created object in DB as well as
+        'self.exported_symbol/function_ids' dictionaries.
+        :param db: DB interface
+        """
+        self.id = db.record_binary_file(self.path)
+        for name in self.exported_symbol_ids.keys():
+            self.exported_symbol_ids[name] = db.record_exported_symbol(self.path, name, is_function=False)
+        for name in self.exported_function_ids.keys():
+            self.exported_function_ids[name] = db.record_exported_symbol(self.path, name, is_function=True)
+
+
+@dataclass
+class Symlink:
+    path: Path
+    target_path: Path
+    target_id: int
+    id: int = None
+
+    @property
+    def name(self):
+        return self.path.name
+
+    def record_in_db(self, db: DBInterface) -> None:
+        """
+        Record the symlink inside the given db as its link to its target.
+        Update 'self.id' with the id of the created object.
+        :param db: DB interface
+        """
+        self.id = db.record_symlink(self.path)
+        db.record_symlink_target(self.id, self.target_id)
+
+
+class Mapper:
+    def __init__(self, root_directory: Path, db: DBInterface):
+        """
+        :param root_directory: directory containing the filesystem to map
+        :param db: interface to the DB
+        """
+        self.root_directory = root_directory
+        self.db_interface = db
+        self.binary_names: dict[str, list[ELFBinary]] = dict()
+        self.binary_paths: dict[Path, ELFBinary] = dict()
+        self.symlink_names: dict[str, list[Symlink]] = dict()
+        self.symlink_paths: dict[Path, Symlink] = dict()
+
+    def _map_binaries(self) -> None:
+        """
+        Iterate all the subdirectories of 'self.root_dir' to find all the binaries.
+        Add them to the DB and create the associated ELFBinary objects.
+        This function updates the fields 'self.binary_paths' and 'self.binary_names'
+        which are respectively binary paths and names dictionaries pointing on
+        the created ELFBinary objects.
+        It adds the binaries into the DB.
+        """
+        for path in filter(lambda p: p.is_file() and lief.is_elf(str(p)),
+                           self.root_directory.rglob('*')):
+            elf_object = ELFBinary(path)
+            elf_object.record_in_db(self.db_interface)
+            self.binary_paths[elf_object.path] = elf_object
+            if elf_object.name in self.binary_names:
+                self.binary_names[elf_object.name].append(elf_object)
+            else:
+                self.binary_names[elf_object.name] = [elf_object]
+
+    def _map_symlinks(self) -> None:
+        """
+        Iterate all the subdirectories of 'self.root_dir' to find all the symlinks.
+        Resolve them and if they point on an ELF file, add them to the DB and create
+        the associated Symlink objects. Also add in db a link between the Symlink object
+        and the ELFBinary object corresponding to its target.
+        This function updates the fields 'self.symlink_paths' and 'self.symlink_names'
+        which are respectively symlink paths and names dictionaries pointing on
+        the created Symlink objects.
+        It adds the Symlinks into the DB.
+        """
+        for path in filter(lambda p: p.is_symlink(), self.root_directory.rglob('*')):
+            target = path.readlink()
+            if target.is_absolute():
+                target = self.root_directory.joinpath(target.relative_to('/'))
+            else:
+                target = path.resolve()
+            if not target.exists() or target == Path('/dev/null') or not lief.is_elf(str(target)):
+                pass
+            elif target.is_relative_to(self.root_directory) and target in self.binary_paths:
+                target_obj = self.binary_paths[target]
+                symlink_obj = Symlink(path, target_obj.path, target_obj.id)
+                symlink_obj.record_in_db(self.db_interface)
+                self.symlink_paths[path] = symlink_obj
+                if symlink_obj.name in self.symlink_names:
+                    self.symlink_names[symlink_obj.name].append(symlink_obj)
+                else:
+                    self.symlink_names[symlink_obj.name] = [symlink_obj]
+            else:
+                print(f"cannot resolve '{path.name}': path '{target} does not exist'")
+
+    def _map_lib_imports(self) -> None:
+        """
+        Iterate over all the binaries (already mapped) and resolve there library
+        imports using the following heuristics:
+         - look for a binary which have the looked name, if there is a match,
+           it is considered as solved, if there is several matches, no mapping is
+           done and a warning log is printed
+         - if no binary match, the same heuristics are applied to symlinks
+        This function update the DB with the import links found and each binary's
+        'imported libs' field with the corresponding ElfBinary objects (or the
+        targeted ELFBinary object in the case of a Symlink)
+        """
+        for path, binary in self.binary_paths.items():
+            for lib_name in binary.imported_lib_names:
+                if lib_name in self.binary_names:
+                    if len(self.binary_names[lib_name]) != 1:
+                        print(f"[lib imports] {path}: several matches for importing lib {lib_name}, not put into DB")
+                    else:
+                        lib_obj = self.binary_names[lib_name][0]
+                        self.db_interface.record_import(binary.id, lib_obj.id)
+                        binary.imported_libs.append(lib_obj)
+                elif lib_name in self.symlink_names:
+                    if len(self.symlink_names[lib_name]) != 1:
+                        print(f"[lib imports] {path}: several matches for importing lib {lib_name}, not put into DB")
+                    else:
+                        sym_obj = self.symlink_names[lib_name][0]
+                        self.db_interface.record_import(binary.id, sym_obj.id)
+                        binary.imported_libs.append(self.binary_paths[sym_obj.target_path])
+                else:
+                    print(f"[lib imports] {path}: lib '{lib_name}' not found in DB")
+
+    def _map_symbol_imports(self) -> None:
+        """
+        Iterate over all the binaries (already mapped) and resolve their symbols.
+        This function update the DB with the import links found.
+        """
+        for path, binary in self.binary_paths.items():
+            for func_name in binary.imported_symbols:
+                found = False
+                if len(func_name.split('@@')) == 2:  # symbols with a specific version
+                    symb_name, symb_version = func_name.split('@@')
+                    if symb_version in binary.version_requirement:
+                        for lib_name in binary.version_requirement[symb_version]:
+                            if lib_name not in self.binary_names:
+                                print(f"[symbol imports] {path}: lib '{lib_name}' not found in DB")
+                            elif len(self.binary_names[lib_name]) > 1:
+                                print(
+                                    f"[symbol imports] {path}: several matches for importing lib {lib_name}, not put into DB")
+                            else:
+                                lib = self.binary_names[lib_name][0]
+                                if symb_name in lib.exported_symbol_ids:
+                                    self.db_interface.record_import(binary.id, lib.exported_symbol_ids[symb_name])
+                                    found = True
+                                elif symb_name in lib.exported_function_ids:
+                                    self.db_interface.record_import(binary.id, lib.exported_function_ids[symb_name])
+                                    found = True
+                else:
+                    for lib in binary.imported_libs:
+                        if func_name in lib.exported_symbol_ids:
+                            self.db_interface.record_import(binary.id, lib.exported_symbol_ids[func_name])
+                            found = True
+                            break
+                        elif func_name in lib.exported_function_ids:
+                            self.db_interface.record_import(binary.id, lib.exported_function_ids[func_name])
+                            found = True
+                            break
+                if found is False:
+                    print(f"[symbol imports] {binary.name}: cannot resolve {func_name}")
+
+    def map(self) -> None:
+        """
+        Map all the content of 'self.root_directory', in the order:
+        - binaries;
+        - symlinks (and resolve them);
+        - lib imports;
+        - symbol imports.
+        It updates the fields and the DB
+        """
+        self._map_binaries()
+        print('[map] Binaries mapping done.')
+        self._map_symlinks()
+        print('[map] Symlinks mapping done.')
+        self._map_lib_imports()
+        print("[map] Binaries' lib imports mapping done.")
+        self._map_symbol_imports()
+
+
+@click.command('pyrrha',
+               help='Map a filesystem into a sourcetrail compatible db.')
+@click.option('--db',
+              help='Sourcetrail DB file path (.srctrldb).',
+              type=click.Path(file_okay=True, dir_okay=True, path_type=Path),
+              default=Path() / 'pyrrha.srctrldb',
+              show_default=True)
+@click.argument('root_directory',
+                # help='Path of the directory containing the filesystem to map.',
+                type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path))
+def pyrrha(root_directory, db):
+    db_interface = DBInterface(db)
+    root_directory = root_directory.absolute()
+
+    mapper = Mapper(root_directory, db_interface)
+    mapper.map()
+
+    db_interface.close()
+
+
+if __name__ == '__main__':
+    pyrrha()
