bdac233d284e697eebb99cd962dde0a620fdde6a
Eloïse Brocas <ebrocas@quarkslab.com>
2023-05-29 16:34:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Pyrrha
+
+Pyrrha is a filesystem mapper which cartographies firmwares more exactly,their ELF binaries (and the associated symlinks) with their relations (exports/imports).
+It uses the open-source code source explorer (for c/cpp, Python, and Java) Sourcetrail to provide user the possibility to navigate and search in the resulting firmware mapping.
+
+## Installation
+The installation is done in two parts:
+- installing `Pyrrha` (as a Python module);
+- installing `Sourcetrail` to be able to visualize Pyrra results.
+
+### Sourcetrail installation
+SOurcetrail can be installed using [its last realease](https://github.com/CoatiSoftware/Sourcetrail/releases/tag/2021.4.19) and its [documentation](https://github.com/CoatiSoftware/Sourcetrail/releases/tag/2021.4.19).
+
+### Pyrrha installation
+Pyrrha's installation compiles [SourcetrailDB](https://github.com/CoatiSoftware/SourcetrailDB) (Sourcetrail SDK) which require to have:
+- `Cmake` >= 2.6;
+- C++-Compiler with C++11 support.
+
+#### Linux
+It is recommended to install the Python package inside a virtualenv.
+```python
+$ pip install '.'
+```
+
+#### Windows
+Not tested.
+
+#### macOS
+Not tested.
+
+## Usage
+First, create your db with `pyrrha`.
+    ```commandline
+    Usage: pyrrha [OPTIONS] ROOT_DIRECTORY
+    
+      Map a filesystem into a sourcetrail compatible db.
+    
+    Options:
+      --db PATH  Sourcetrail DB file path (.srctrldb).  [default: pyrrha.srctrldb]
+      --help     Show this message and exit.
+    ```
+Then, open the resulting project with `sourcetrail`.
---
+++ setup.py
@@ -0,0 +1,46 @@
+import shutil
+import subprocess
+from pathlib import Path
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from tempfile import mkdtemp
+from zipfile import ZipFile
+
+CURRENT_DIR = Path(__file__).parent
+ZIP_PATH = CURRENT_DIR / 'dependencies' / 'SourcetrailDB-4.db25.p1.zip'
+EXTRACTION_DIR = Path(mkdtemp())
+
+
+class ZipCMakeExtension(Extension):
+    def __init__(self, name: str) -> None:
+        super().__init__(name, sources=[])
+        zip_file = ZipFile(ZIP_PATH)
+        zip_file.extractall(path=EXTRACTION_DIR)
+        self.sourcedir = EXTRACTION_DIR.resolve().absolute().joinpath('SourcetrailDB-4.db25.p1')
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        cmake_args = ['-DBUILD_BINDINGS_PYTHON=ON']
+        build_args = ['--target', '_bindings_python']
+
+        self.build_temp = Path(self.build_temp)
+        if not self.build_temp.exists():
+            self.build_temp.mkdir(parents=True)
+
+        subprocess.check_call(['cmake', f'{ext.sourcedir}'] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+        shutil.copy2(self.build_temp / 'bindings_python' / 'sourcetraildb.py', self.build_lib)
+        shutil.copy2(self.build_temp / 'bindings_python' / '_sourcetraildb.so', self.build_lib)
+
+        shutil.rmtree(EXTRACTION_DIR)
+        shutil.rmtree(self.build_temp)
+
+setup(
+    ext_modules=[
+        ZipCMakeExtension('sourcetraildb')
+    ],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+)
