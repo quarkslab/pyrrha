26adb901058aee630913e98f5174e93ce9f94ac2
Eloïse Brocas <ebrocas@quarkslab.com>
2023-07-10 11:44:37 +0200

    mapper, main, install: add possibility to add multiple mappers
    closes #11

---
+++ src/pyrrha/__main__.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import click
+
+from .db import DBInterface
+from .filesystem import FileSystemMapper
+
+
+@click.group()
+def pyrrha():
+    pass
+
+
+"""
+ Filesystem mapper.
+ Map ELF files, their imports and their exports.
+ Also map symlinks which target ELF files.
+"""
+@pyrrha.command('fs',
+               help='Map a filesystem into a sourcetrail-compatible db.')
+@click.option('--db',
+              help='Sourcetrail DB file path (.srctrldb).',
+              type=click.Path(file_okay=True, dir_okay=True, path_type=Path),
+              default=Path() / 'pyrrha.srctrldb',
+              show_default=True)
+@click.argument('root_directory',
+                # help='Path of the directory containing the filesystem to map.',
+                type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path))
+def fs(db, root_directory):
+    db_interface = DBInterface(db)
+    root_directory = root_directory.absolute()
+
+    mapper = FileSystemMapper(root_directory, db_interface)
+    mapper.map()
+
+    db_interface.close()
+
+
+if __name__ == '__main__':
+    pyrrha()
