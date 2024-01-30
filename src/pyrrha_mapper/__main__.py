e937d195d04ee12513f2ed58bd41e05aa7b01d83
Eloïse Brocas <ebrocas@quarkslab.com>
2024-01-30 16:46:59 +0100

    all: update license to current year

--- src/pyrrha_mapper/__main__.py
+++ src/pyrrha_mapper/__main__.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright 2023-2024 Quarkslab
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 import coloredlogs
 import logging
 import multiprocessing
@@ -50,6 +66,7 @@ def setup_logs(is_debug_level: bool) -> None:
                                       'warning' : {'color': 'yellow'}, 'error': {'color': 'red'},
                                       'critical': {'bold': True, 'color': 'red'}},
                         field_styles={'asctime': {'color': 'green'}, 'levelname': {'bold': True}}, **log_format)
+
 
 def setup_db(db_path, overwrite_db: bool = True) -> SourcetrailDB:
     """
