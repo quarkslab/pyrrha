50de6a86ffaee158842f98b870e080157708a3c8
Robin David <dev.robin.david@gmail.com>
2025-01-22 23:48:02 +0100

    in arbitrary mode one entry is fine

--- src/pyrrha_mapper/mappers/intercg/fwmapper.py
+++ src/pyrrha_mapper/mappers/intercg/fwmapper.py
@@ -218,7 +218,7 @@ def map_firmware(
                                             choice_bin = table[target][val]
                                             resolve_cache.add(choice_bin)
                                             served_by = [choice_bin]
-                                elif len(served_by) > 1 and resolver == ResolveDuplicateOption.ARBITRARY:
+                                elif len(served_by) >= 1 and resolver == ResolveDuplicateOption.ARBITRARY:
                                     arb_choice = table[target][0]
                                     resolve_cache.add(arb_choice)
                                     served_by = [arb_choice]  # Take the first one
