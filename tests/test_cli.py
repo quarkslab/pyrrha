f02b16b0e0af2dd5cce51b63ec153fc3d5e41856
Eloïse Brocas <ebrocas@quarkslab.com>
2025-02-28 13:55:47 +0100

    tests: update tests to new FS object which can load export results

---
+++ tests/test_cli.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+#  Copyright 2023-2025 Quarkslab
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""Functionnal test from the CLI."""
+
+from pathlib import Path
+
+import pytest
+from click import BaseCommand
+from click.testing import CliRunner, Result
+
+from pyrrha_mapper import pyrrha
+from pyrrha_mapper.fs.filesystem_objects import FileSystem
+
+
+class TestCLI:
+    """Tests to check that the CLI works and display correct messages."""
+
+    COMMAND: BaseCommand = pyrrha
+    SUBCOMMANDS = pyrrha.commands
+
+    def test_usage(self):
+        """Display correctly main usage."""
+        runner = CliRunner()
+        res_short = runner.invoke(self.COMMAND, ["-h"])
+        res_long = runner.invoke(self.COMMAND, ["--help"])
+        for res in [res_long, res_short]:
+            assert res.exit_code == 0
+            assert res.output.startswith(f"Usage: {self.COMMAND.name}")
+        assert res_short.output == res_long.output
+
+    @pytest.mark.parametrize("subcommand", SUBCOMMANDS)
+    def test_subcommand_usage(self, subcommand: str):
+        """Display correctly submcommand usage."""
+        runner = CliRunner()
+        res_short = runner.invoke(self.COMMAND, [subcommand, "-h"])
+        res_long = runner.invoke(self.COMMAND, [subcommand, "--help"])
+        for res in [res_long, res_short]:
+            assert res.exit_code == 0
+            assert res.output.startswith(f"Usage: {self.COMMAND.name} {subcommand}")
+        assert res_short.output == res_long.output
+
+
+class TestFSMapper:
+    """Main functional test class for the FS mapper. Tests are done from the CLI."""
+
+    COMMAND = pyrrha
+    SUBCOMMAND = "fs"
+    FW_TEST_PATH = Path(__file__).parent / "test_fw"
+    FW_TEST_LD = Path("/lib/ld-linux.so.3")
+    FW_TEST_BIN_PATHS = {
+        FW_TEST_LD,
+        Path("/lib/libc.so.6"),
+        Path("/lib/libcrypto.so.1.1"),
+        Path("/lib/libdl.so.2"),
+        Path("/lib/libpthread.so.0"),
+        Path("/lib/libssl.so.1.1"),
+        Path("/bin/openssl"),
+    }
+    FW_TEST_SYMLINKS_PATHS = {Path("/lib/libssl.so")}
+
+    @pytest.fixture
+    def db_path(self, tmp_path_factory) -> Path:
+        """Generate the path for DB file."""
+        tmp = tmp_path_factory.mktemp("db", numbered=True)
+        return tmp / "test.srctrlprj"
+
+    @pytest.mark.parametrize("nb_thread", [1, 16])
+    def test_numbat_project_creation(self, nb_thread, db_path):
+        """Two files are generated with correct extensions."""
+        runner = CliRunner()
+        args = [
+            self.SUBCOMMAND,
+            "--db",
+            f"{db_path}",
+            "-j",
+            nb_thread,
+            f"{self.FW_TEST_PATH}",
+        ]
+        res = runner.invoke(self.COMMAND, args)
+        assert res.exit_code == 0
+        assert db_path.exists()
+        assert db_path.with_suffix(".srctrldb").exists()
+        assert db_path.with_suffix(".srctrlprj").exists()
+
+    @pytest.fixture
+    def export_path(self, db_path: Path) -> Path:
+        """Compute path for the exported JSON."""
+        return db_path.with_suffix(".json")
+
+    @pytest.fixture(params=[1, 16])
+    def export_res(self, db_path: Path, request) -> Result:
+        """Run Pyrrha with export activated."""
+        runner = CliRunner()
+        args = [
+            self.SUBCOMMAND,
+            "-e",
+            "--db",
+            f"{db_path}",
+            "-j",
+            request.param,
+            f"{self.FW_TEST_PATH}",
+        ]
+        res = runner.invoke(self.COMMAND, args)
+        return res
+
+    def test_export_creation(self, export_res: Result, export_path: Path) -> None:
+        """Export file exist."""
+        assert export_res.exit_code == 0
+        assert export_path.exists()
+
+    @pytest.fixture
+    def export_dump(self, export_res, export_path: Path) -> FileSystem:
+        """Load JSON export into a FileSystem object."""
+        return FileSystem.from_json_export(export_path)
+
+    def test_export_format(self, export_dump: FileSystem) -> None:
+        """JSON export is loaded as a Filesystem object."""
+        assert isinstance(export_dump, FileSystem)
+
+    def test_binary_list(self, export_dump: FileSystem) -> None:
+        """Firmware binaries are present in results."""
+        assert {
+            _bin.path for _bin in export_dump.iter_binaries()
+        } == self.FW_TEST_BIN_PATHS
+
+    def test_symlink_list(self, export_dump: FileSystem) -> None:
+        """Firmware symlinks are present in results."""
+        assert {
+            sym.path for sym in export_dump.iter_symlinks()
+        } == self.FW_TEST_SYMLINKS_PATHS
+
+    @pytest.mark.parametrize("bin_path", FW_TEST_BIN_PATHS)
+    def test_exported_symbols(self, bin_path: Path, export_dump: FileSystem) -> None:
+        """Exported symbols exist for each binary of the firware."""
+        _bin = export_dump.get_binary_by_path(bin_path)
+        assert len(list(_bin.iter_exported_symbols())) > 0
+
+    @pytest.mark.parametrize("bin_path", FW_TEST_BIN_PATHS)
+    def test_dependencies(self, bin_path: Path, export_dump: FileSystem) -> None:
+        """Imported libraries exist for each binary of the firware except ldd."""
+        _bin = export_dump.get_binary_by_path(bin_path)
+        if bin_path == self.FW_TEST_LD:
+            assert len(list(_bin.iter_imported_libraries())) == 0
+        else:
+            assert len(list(_bin.iter_imported_libraries())) > 0
+
+    @pytest.mark.parametrize("bin_path", FW_TEST_BIN_PATHS)
+    def test_resolved_dependencies(
+        self, bin_path: Path, export_dump: FileSystem
+    ) -> None:
+        """Imported libraries correspond to a binary object."""
+        _bin = export_dump.get_binary_by_path(bin_path)
+        assert len(list(_bin.iter_imported_libraries())) == len(
+            _bin.imported_library_names
+        )
+
+    @pytest.mark.parametrize("bin_path", FW_TEST_BIN_PATHS)
+    def test_imported_symbols(self, bin_path: Path, export_dump: FileSystem) -> None:
+        """Imported symbols exist for each binary of the firware except ldd."""
+        _bin = export_dump.get_binary_by_path(bin_path)
+        if bin_path == self.FW_TEST_LD:
+            assert len(_bin.imported_symbol_names) == 0
+        else:
+            assert len(_bin.imported_symbol_names) > 0
+
+    @pytest.mark.parametrize("bin_path", FW_TEST_BIN_PATHS)
+    def test_resolved_imported_symbols(
+        self, bin_path: Path, export_dump: FileSystem
+    ) -> None:
+        """Imported symbols correspond to a symbol object."""
+        _bin = export_dump.get_binary_by_path(bin_path)
+        assert len(list(_bin.iter_imported_symbols())) == len(
+            _bin.imported_symbol_names
+        )
