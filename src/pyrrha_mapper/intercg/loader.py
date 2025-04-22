a76d628d7dde2b761975108d94c1a37b608605af
Eloïse Brocas <ebrocas@quarkslab.com>
2025-04-22 18:51:40 +0200

    intercg: refacto
    Signed-off-by: Eloïse Brocas <ebrocas@quarkslab.com>

--- src/pyrrha_mapper/intercg/loader.py
+++ src/pyrrha_mapper/intercg/loader.py
@@ -1,16 +1,15 @@
 """Load information used by InterCGMapper from the files on the disk."""
+
 import logging
-import re
-from collections import defaultdict
 
 # third-party imports
-import lief
 from quokka import Program
 from quokka.exc import ChunkMissingError
 from quokka.types import FunctionType
 
 # local imports
-from pyrrha_mapper import Binary
+from pyrrha_mapper.common import Binary, Symbol
+from pyrrha_mapper.exceptions import FsMapperError
 
 QUOKKA_EXT = ".quokka"
 
@@ -24,8 +23,8 @@ QUOKKA_EXT = ".quokka"
 """
 
 
-def load_program(binary: Binary) -> None:
-    """Load a binary file and its quokka file into a Binary object.
+def load_program(binary: Binary) -> dict[Symbol, list[str]]:
+    """Create a Binary object from a given file using lief and quokka.
 
     It modifies the provided binary object in place.
 
@@ -33,34 +32,33 @@ def load_program(binary: Binary) -> None:
     1. load the program object
     2. use lief to extract exported symbols (handle conflicts with IDA names)
     3. checks if exported functions have been missed by IDA (but referenced in LIEF)
-    4. Mangle the call graph to make external call to .PLT to directly jump on the 
-       external symbol
+    4. Mangle the call graph to make external call to .PLT to directly jump on the
+        external symbol
 
-    :param binary: binary file from which to load additional data
-    :param exec_file: path to the corresponding binary file
-    :return: a Program object corresponding to the given binary
+    raise: FsMapperError if cannot load it
+
+    :param binary: a Binary object that will be completed
     """
-    # Load Quokka file to retrieve Program object
-    exec_file = binary.real_path
+    file_path = binary.real_path
+    if file_path is None:
+        raise FsMapperError()
+
     quokka_file = binary.auxiliary_file(append=QUOKKA_EXT)
     try:
         if quokka_file.exists():
-            program = Program(quokka_file, exec_file)
+            program: Program | None = Program(quokka_file, file_path)
         else:
-            program = Program.from_binary(exec_file, quokka_file, timeout=3600)
+            program = Program.from_binary(file_path, quokka_file, timeout=3600)
     except ChunkMissingError as e:
-        raise SyntaxError() from e
+        raise FsMapperError() from e
     if program is None:
-        raise SyntaxError()
-
-    # Load exports / mangling info from
-    get_lief_exports_mapping(binary)
+        raise FsMapperError()
 
     # Load the call graph
-    compute_call_graph(binary, program)
+    return compute_call_graph(binary, program)
 
 
-def compute_call_graph(binary: Binary, program: Program) -> None:
+def compute_call_graph(binary: Binary, program: Program) -> dict[Symbol, list[str]]:
     """Compute the call graph of the program using Quokka/Binexport.
 
     It fill the call attribute of binary.
@@ -69,25 +67,41 @@ def compute_call_graph(binary: Binary, program: Program) -> None:
     :param program: Program object in which to extract data
     """
     # Call graph fun_name -> [callee_name1, callee_name2]
-    call_graph: dict[str, list[str]] = {}
+    call_graph: dict[Symbol, list[str]] = {}
+
+    exports: dict[int, list[Symbol]] = dict()
+    for s in binary.iter_exported_functions():
+        if s.addr is None:
+            continue
+        elif s.addr not in exports:
+            exports[s.addr] = [s]
+        else:
+            exports[s.addr].append(s)
 
     # Fill temporary dict
-    _inter_cg = {}  # addr: (mangled_name, pp_name, addr, typ, calls: list[int])
+    _inter_cg: dict[
+        int, tuple[Symbol, str, str, int, FunctionType, list[int]]
+    ] = {}  # addr: (pyrrha symbol, mangled_name, pp_name, addr, typ, calls: list[int])
     for f_addr, f in program.items():
         name = f.mangled_name  # as computed by IDA
 
-        if exports.get(f_addr) or exports.get(
-            f_addr + 1
+        if (
+            f_addr in exports or f_addr + 1 in exports
         ):  # If the function is somewhat exported (and visible in LIEF)
-            res = exports.get(
-                f_addr, exports.get(f_addr + 1)
+            all_symbs = exports.get(
+                f_addr, exports[f_addr + 1]
             )  # In THUMB mode address is address+1
-            canonical_name, all_names = res
-            if name != canonical_name:
-                logging.info(f"change fun name: {name} -> {canonical_name}")
-                name = canonical_name  # In multiple names just select the canonical one
 
+            canonical = disambiguate_export(all_symbs)
+            if name != canonical.name:
+                logging.info(f"change fun name: {name} -> {canonical.name}")
+                name = canonical.name  # In multiple names just select the canonical one
+            f_symb = canonical
+        else:
+            f_symb = Symbol(name=name, demangled_name=f.name, is_func=True, addr=f_addr)
+            binary.add_function(f_symb)  # FIXME : c'est bien une fonction interne ???
         _inter_cg[f_addr] = (
+            f_symb,
             name,
             f.name,
             f_addr,
@@ -97,27 +111,28 @@ def compute_call_graph(binary: Binary, program: Program) -> None:
 
     # Check if some exports don't have any associated function (not detected by IDA)
     for exp_addr in exports.keys() - program.keys():
-        canon, allnames = exports[exp_addr]
+        all_symbs = exports[exp_addr]
+        canon = disambiguate_export(all_symbs)
         if p_fun := program.get(
             exp_addr - 1
         ):  # IDA keeps ARM address while LIEF use THUMB addresses
-            if p_fun.mangled_name in allnames:  # Check that we have a match on names
+            if p_fun.mangled_name in [
+                s.name for s in all_symbs
+            ]:  # Check that we have a match on names
                 continue
         # else case
         logging.info(
-            f"export {canon}: {hex(exp_addr)} address not found in program (add one)."
+            f"export {canon.name}: {hex(exp_addr)} address not found in program (add)."
         )
         call_graph[canon] = []
 
     # Iterate back the temporary dict to fill the real call graph
-    # The deal here is to fast-forward call to imported function directly on the 
+    # The deal here is to fast-forward call to imported function directly on the
     # imported symbol and not on the PLT (to make the graph more straightforward)
-    for f_name, f_pp_name, f_addr, f_type, calls in _inter_cg.values():
+    for f_symb, f_name, _, f_addr, f_type, calls in _inter_cg.values():
         if f_type in [FunctionType.NORMAL, FunctionType.LIBRARY]:
-            call_graph[f_name] = []  # add entry in call graph
-
             # Iter calls (ignore calls that are not pointing to a function!)
-            for c_name, c_pp_name, c_addr, c_type, c_calls in [
+            for _, c_name, _, c_addr, c_type, c_calls in [
                 _inter_cg[x] for x in calls if x in _inter_cg
             ]:
                 if c_name:  # Has a true name
@@ -136,102 +151,56 @@ def compute_call_graph(binary: Binary, program: Program) -> None:
                                 FunctionType.IMPORTED,
                                 FunctionType.EXTERN,
                             ]:
-                                call_graph[f_name].append(
+                                call_graph[f_symb].append(
                                     c_name
                                 )  # Keep the name of the thunk "strcpy, sprintf"
                             else:  # Forward the call to the underlying function name
-                                call_graph[f_name].append(sub_callee[0])
+                                call_graph[f_symb].append(sub_callee[0].name)
                         else:
-                            call_graph[f_name].append(c_name)  # Add it normally
+                            call_graph[f_symb].append(c_name)  # Add it normally
+                    # Add it normally
                     else:  # In all other cases
-                        call_graph[f_name].append(c_name)  # Add it normally
+                        call_graph[f_symb].append(c_name)  # Add it normally
                 else:  # ignore function without name
                     logging.warning(
-                        f"[{program.name}] {f_name} calls a function without name (at {c_addr:#08x})"
+                        f"[{program.name}] {f_name} calls a function without name \
+(at {c_addr:#08x})"
                     )
 
         # If thunk AND exported still keep it (for later resolution)
         elif f_type == FunctionType.THUNK and (
             (f_addr in exports) or (f_addr + 1 in exports)
         ):
-            call_graph[f_name] = [_inter_cg[x][0] for x in calls if x in _inter_cg]
+            call_graph[f_symb] = [_inter_cg[x][1] for x in calls if x in _inter_cg]
 
         else:  # THUNK, IMPORTED, EXTERN (not included in call graph)
             pass
 
-    # Change exports dict to be: name_mangled -> name_canonical
-    exports = {name: x[0] for x in exports.values() for name in x[1]}
-
-    # Add to demangled mapping, IDA functions names from Quokka
-    demangled.update({x[0]: x[1] for x in _inter_cg.values()})
-
-    return Binary(
-        -1,
-        exec_file.name,
-        str(exec_file),
-        calls=call_graph,
-        exports=exports,
-        demangled=demangled,
-    )
+    return call_graph
 
 
-def get_lief_exports_mapping(
-    binary: Binary,
-) -> tuple[dict[int, tuple[str, list[str]]], dict[str, str]]:
-    """
-    Get the export mapping of a file as given by LIEF.
-
-    :param binary: binary object to update
-    :return: tuple of exported symbols, and demangled names dictionary
-    """
-    # FIXME: Try to see if there is something to do to unify with
-    # FIXME: what is being done in FS-mapper
-    # FIXME: it should fills both exports and demangled !
-    # TODO: Maybe move to FS-mapper ?
-
-    if not re.match(".*\.so(\.\d*)*$", str(exec_file)):
-        return (
-            {},
-            {},
-        )  # Only gather exports for .so files. Assume none are exported for other bins
-
-    p = lief.parse(exec_file)
-
-    # Fill dict with exported symbols
-    exports = defaultdict(
-        list
-    )  # list there can be multiple symbols on the same address
-    for s in (s for s in p.symbols if s.is_function and s.exported):
-        exports[s.value].append(s.name)  # !! Add mangled name !
-
-    # Redefine exports mapping to choose a single name for all aliases
-    exports = {k: (disambiguate_export(names), names) for k, names in exports.items()}
-
-    demangled = {
-        x.name: x.demangled_name for x in p.symbols if x.is_function and x.exported
-    }
-    return exports, demangled
-
-
-def disambiguate_export(names: list[str]) -> str:
-    if len(names) == 1:
-        return names[0]  # If only one no ambiguity
+def disambiguate_export(symbs: list[Symbol]) -> Symbol:
+    """Given a list of symbols associated with one address, chose one."""
+    if len(symbs) == 1:
+        return symbs[0]  # If only one no ambiguity
 
     chosen = None
-    for name in names:
-        if name.startswith("_"):
+    for symb in symbs:
+        if symb.name.startswith("_"):
             continue
         if chosen is None:
-            chosen = name
-        elif chosen == name:
+            chosen = symb
+        elif chosen == symb:
             continue
         else:
             # print(f"multiple options for name: {chosen}, {name}")
-            if len(name) < len(chosen):
-                chosen = name
+            if len(symb.name) < len(chosen.name):
+                chosen = symb
 
     # all exports starts with _
     if chosen is None:
-        logging.warning(f"cannot disambiguate: {names} (select shortest name)")
-        chosen = min(names, key=len)
+        logging.warning(
+            f"cannot disambiguate: {[s.name for s in symbs]} (select shortest name)"
+        )
+        chosen = min(symbs, key=lambda x: len(x.name))
     return chosen
