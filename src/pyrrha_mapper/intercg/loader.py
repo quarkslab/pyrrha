2b0d1b0fb7cce784f31e47a0ab9ca2b1ac8ed639
Eloïse Brocas <ebrocas@quarkslab.com>
2025-05-21 16:18:22 +0200

    intercg: split call graph function in several ones and optimize thunk treatment

--- src/pyrrha_mapper/intercg/loader.py
+++ src/pyrrha_mapper/intercg/loader.py
@@ -81,11 +81,76 @@ def load_program(binary: Binary, log_prefix: str = "") -> dict[Symbol, list[str]
 
 class _FuncData(NamedTuple):
     symbol: Symbol
-    name: str
-    demangled_name: str
-    addr: int
     type: FunctionType
     calls: list[int]
+    callers: list[int]
+
+    @property
+    def name(self) -> str:
+        return self.symbol.name
+
+    @property
+    def demangled_name(self) -> str:
+        return self.symbol.demangled_name
+
+    @property
+    def addr(self) -> int:
+        assert self.symbol.addr
+        return self.symbol.addr
+
+
+def _generate_calls_list(
+    func: _FuncData, call_graph: dict[int, _FuncData], log_prefix: str
+) -> list[str]:
+    """Given a function return its call list.
+
+    It only contains functions that are contained in the call graph and have a name.
+    """
+    res = list()
+    for c in [call_graph[x] for x in func.calls if x in call_graph]:
+        if c.name:  # Has a true name
+            res.append(c.name)  # Add it normally
+        else:  # ignore function without name
+            logging.warning(
+                f"{log_prefix}: {func.symbol} calls a function without name (at {c.addr:#08x})"
+            )
+    return res
+
+
+def combine_program_analysis_binary(
+    binary: Binary, program: Program, log_prefix: str
+) -> dict[int, _FuncData]:
+    """Combine program and binary objects by computing useful data.
+
+    It updates binary object if new functions are determined.
+
+    :param binary: binary object to update, contain data already analyzed
+    :param program: Program object in which to extract data
+    :return: a dict [addr, FuncData object associated to this address]
+    """
+    exports = binary.exported_funcs_by_addr
+    program_data: dict[int, _FuncData] = {}
+    for f_addr, f in program.items():
+        if f_addr in exports or f_addr + 1 in exports:  # function exported (and visible in LIEF)
+            all_symbs = exports.get(
+                f_addr, exports.get(f_addr + 1, [])
+            )  # In THUMB mode address is address+1
+            f_symb = disambiguate_export(all_symbs, log_prefix)
+            if f.name != f_symb.demangled_name:
+                logging.debug(f"{log_prefix}: change fun name {f.name} -> {f_symb.demangled_name}")
+            if len(all_symbs) > 1:  # all the symbols will point on the chosen one
+                map(lambda x: binary.replace_function(f_symb, x, True), all_symbs)
+        else:
+            f_symb = Symbol(name=f.mangled_name, demangled_name=f.name, is_func=True, addr=f_addr)
+            binary.add_function(f_symb)
+
+        program_data[f_addr] = _FuncData(
+            symbol=f_symb,
+            type=f.type,
+            calls=list(set(x.start for x in f.calls)),
+            callers=list(set(x.start for x in f.callers)),
+        )
+    return program_data
 
 
 def compute_call_graph(
@@ -95,123 +160,79 @@ def compute_call_graph(
 
     It fill the call attribute of binary.
 
-    :param binary: binary object to fill
+    :param binary: binary object to update, contain data already analyzed
     :param program: Program object in which to extract data
     """
+
+    def _nb_initial_underscore(x: str) -> int:
+        return len(x) - len(x.strip("_"))
+
     # Call graph fun_name -> [callee_name1, callee_name2]
     call_graph: dict[Symbol, list[str]] = {}
+    exports = binary.exported_funcs_by_addr
 
-    exports: dict[int, list[Symbol]] = dict()
-    for s in binary.iter_exported_functions():
-        if s.addr is None:
-            continue
-        elif s.addr not in exports:
-            exports[s.addr] = [s]
-        else:
-            exports[s.addr].append(s)
-
-    # Fill temporary dict
-    _inter_cg: dict[int, _FuncData] = {}
-    for f_addr, f in program.items():
-        if (
-            f_addr in exports or f_addr + 1 in exports
-        ):  # If the function is somewhat exported (and visible in LIEF)
-            all_symbs = exports.get(
-                f_addr, exports.get(f_addr + 1, [])
-            )  # In THUMB mode address is address+1
-            canonical = disambiguate_export(all_symbs, log_prefix)
-            if f.name != canonical.demangled_name:
-                logging.debug(
-                    f"{log_prefix}: change fun name {f.name} -> {canonical.demangled_name}"
-                )
-            if len(all_symbs) > 1:
-                for s in all_symbs:  # all the symbols will point on the chosen one
-                    if binary.exported_function_exists(s.name):
-                        binary.add_exported_symbol(canonical, symbol_name=s.name)
-                    else:
-                        binary.add_function(canonical, func_name=s.name)
-            f_symb = canonical
-        else:
-            f_symb = Symbol(name=f.mangled_name, demangled_name=f.name, is_func=True, addr=f_addr)
-            binary.add_function(f_symb)
-        _inter_cg[f_addr] = _FuncData(
-            symbol=f_symb,
-            name=f_symb.name,
-            demangled_name=f_symb.demangled_name,
-            addr=f_addr,
-            type=f.type,
-            calls=list(set(x.start for x in f.calls)),
-        )
+    # Combine program and binary objects by computing useful data
+    program_data = combine_program_analysis_binary(binary, program, log_prefix)
 
     # Check if some exports don't have any associated function (not detected by IDA)
     for exp_addr in exports.keys() - program.keys():
         all_symbs = exports[exp_addr]
         canon = disambiguate_export(all_symbs, log_prefix)
-        if p_fun := program.get(
-            exp_addr - 1
-        ):  # IDA keeps ARM address while LIEF use THUMB addresses
-            if p_fun.mangled_name in [
-                s.name for s in all_symbs
-            ]:  # Check that we have a match on names
+        if p_fun := program.get(exp_addr - 1):
+            # IDA keeps ARM address while LIEF use THUMB addresses
+            if p_fun.mangled_name in [s.name for s in all_symbs]:
+                # Check that we have a match on names
                 continue
         # else case
         logging.debug(
             f"{log_prefix}: export {canon.name}: {hex(exp_addr)} address not found in program(add)."
         )
         call_graph[canon] = []
-        if len(all_symbs) > 1:
-            for s in all_symbs:  # all the symbols will point on the chosen one
-                if binary.exported_function_exists(s.name):
-                    binary.add_exported_symbol(canon, symbol_name=s.name)
-                else:
-                    binary.add_function(canon, func_name=s.name)
+        if len(all_symbs) > 1:  # all the symbols will point on the chosen one
+            map(lambda x: binary.replace_function(canon, x, True), all_symbs)
 
     # Iterate back the temporary dict to fill the real call graph
     # The deal here is to fast-forward call to imported function directly on the
     # imported symbol and not on the PLT (to make the graph more straightforward)
     removed_trampoline: dict[str, str] = dict()
-    for f in _inter_cg.values():
-        call_graph[f.symbol] = []
-        if f.type in [FunctionType.NORMAL, FunctionType.LIBRARY]:
-            # Iter calls (ignore calls that are not pointing to a function!)
-            for c in [_inter_cg[x] for x in f.calls if x in _inter_cg]:
-                if c.name:  # Has a true name
-                    if c.type == FunctionType.THUNK and c.addr not in exports:
-                        if len(c.calls) == 1:  # The callee calls something else (and only one)
-                            if c.calls[0] in _inter_cg:
-                                sub_callee = _inter_cg[c.calls[0]]
-                            else:  # Do not do anything if not pointing to a func
-                                continue
-                            if sub_callee.type in [
-                                FunctionType.IMPORTED,
-                                FunctionType.EXTERN,
-                            ]:
-                                call_graph[f.symbol].append(
-                                    c.name
-                                )  # Keep the name of the thunk "strcpy, sprintf"
-                                removed_trampoline[sub_callee.name] = c.name
-                            else:  # Forward the call to the underlying function name
-                                call_graph[f.symbol].append(sub_callee[0].name)
-                                removed_trampoline[c.name] = sub_callee[0].name
-                        else:
-                            call_graph[f.symbol].append(c.name)  # Add it normally
-                    # Add it normally
-                    else:  # In all other cases
-                        call_graph[f.symbol].append(c.name)  # Add it normally
-                else:  # ignore function without name
-                    logging.warning(
-                        f"{log_prefix}: [{program.name}] {f.symbol} calls a function without"
-                        " name (at {c.addr:#08x})"
-                    )
+    for f in program_data.values():
+        if (
+            f.type in [FunctionType.NORMAL, FunctionType.LIBRARY]
+            # If thunk AND exported or thunk AND call several func, keep it (for later resolution)
+            or (
+                f.type == FunctionType.THUNK
+                and ((f.addr in exports) or (f.addr + 1 in exports) or len(f.calls) > 1)
+            )
+        ):
+            call_graph[f.symbol] = _generate_calls_list(f, program_data, log_prefix)
+            continue
 
-        # If thunk AND exported still keep it (for later resolution)
-        elif f.type == FunctionType.THUNK and ((f.addr in exports) or (f.addr + 1 in exports)):
-            call_graph[f.symbol] = [_inter_cg[x].name for x in f.calls if x in _inter_cg]
+        # Replace thunk calling only one function (and only one)        
+        elif f.type == FunctionType.THUNK and len(f.calls) == 1 and f.calls[0] in program_data:
+            sub_callee = program_data[f.calls[0]]
+            if sub_callee.type in [FunctionType.IMPORTED, FunctionType.EXTERN]:
+                # Keep the name of the thunk "strcpy, sprintf"
+                name, target = sub_callee.name, f.name
+                # in case of nested functions (starting with _, keep the less nested one)
+                if _nb_initial_underscore(target) > _nb_initial_underscore(name):
+                    name, target = target, name
+            else:  # Forward the call to the underlying function name
+                name, target = f.name, sub_callee[0].name
+            # resolve trampoline and update associated dict
+            while target in removed_trampoline:
+                target = removed_trampoline[target]
+            removed_trampoline[name] = target
+            for key, val in removed_trampoline.items():
+                if val == name:
+                    removed_trampoline[key] = target
 
-        else:  # THUNK, IMPORTED, EXTERN (not included in call graph)
+        # If terminal thunk keep it in binary
+        elif f.type == FunctionType.THUNK and len(f.calls) == 0 and len(f.callers) > 0:  
+            continue
+
+        # remove any function not explicitely kept (THUNK, IMPORTED, EXTERN)
+        if binary.get_function_by_name(f.name).addr == f.addr:
             binary.remove_function(f.name)
-            call_graph.pop(f.symbol)
-            pass
 
     return {
         symb: [removed_trampoline[c] if c in removed_trampoline else c for c in calls]
